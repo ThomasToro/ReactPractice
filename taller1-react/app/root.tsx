9d1b07e62ad46a2ec666a430c1e6b3a15979ca4b
David Toro <davidesteban0736@.com>
2026-02-02 20:17:42 -0500

    creacion de sidebar

--- taller1-react/app/root.tsx
+++ taller1-react/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { SideBar } from "./routes/SideBar/SideBar";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,6 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <SideBar/>
         {children}
         <ScrollRestoration />
         <Scripts />
---
+++ taller1-react/app/routes/SideBar/SideBar.tsx
@@ -0,0 +1,25 @@
+
+let David= {git:"https://github.com/DavidToroHrr",telefono:"3138945544",correo:"davidesteban0726@gmail.com",descripcion:"Estudiante de Ingeniería de Sistemas de la Universidad Autónoma de Manizales."}
+
+let Thomas= {git:"",telefono:"3138945544",correo:"correo",descripcion:"Estudiante de Ingeniería de Sistemas de la Universidad Autónoma de Manizales."}
+
+
+
+export function SideBar() {
+
+return (
+    <div id="SideBarNav">
+
+        <img src="" alt="imagen de la persona" />
+        <h1>Nombre: {}</h1>
+        <h3>Git: {}</h3>
+        <h3>Correo {}</h3>
+        <h3>Descripcion {}</h3>
+    </div> 
+)
+
+    
+
+
+
+}
